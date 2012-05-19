89486be0d3410ffe59928a7ab613f6950f099dc0
Rafael Bandeira <rafaelbandeira3@gmail.com>
2012-05-19 00:19:08 -0300

    Silly script to copy everything

---
+++ install.py
@@ -0,0 +1,20 @@
+#! /usr/bin/python
+
+import os
+import shutil
+
+def srcpath(file):
+  return os.path.join(os.getcwd(), file)
+
+def dstpath(file):
+  return os.path.join(os.environ["HOME"], file) 
+
+def shouldcopy(file):
+  return (file.startswith('.') 
+         and not file.endswith('.swp')
+         and not os.path.isdir(srcpath(file)))
+
+for dotfile in os.listdir('.'):
+  if shouldcopy(dotfile):
+    print 'copying ' + dotfile
+    shutil.copy(srcpath(dotfile), dstpath(dotfile))    
