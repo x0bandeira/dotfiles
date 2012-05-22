220ac55725660d82507d0c7598a072ced2bf8d72
Rafael Bandeira <rafaelbandeira3@gmail.com>
2012-05-22 19:07:43 -0300

    ZSH init copy to hold modularized zsh bootstrap scripts

---
+++ .zsh-init/rbenv.sh
@@ -0,0 +1,18 @@
+PATH="$HOME/.rbenv/bin:${PATH}"
+PATH="$HOME/.rbenv/shims:${PATH}"
+
+source "/usr/local/Cellar/rbenv/0.3.0/libexec/../completions/rbenv.zsh"
+rbenv rehash 2>/dev/null
+rbenv() {
+  command="$1"
+  if [ "$#" -gt 0 ]; then
+    shift
+  fi
+
+  case "$command" in
+    shell)
+      eval `rbenv "sh-$command" "$@"`;;
+    *)
+      command rbenv "$command" "$@";;
+  esac
+}
